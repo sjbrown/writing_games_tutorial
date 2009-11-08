eea0d00c3a0af7ec9cbdc45f019cde58ef22b5b2
Shandy Brown <github@ezide.com>
2009-11-08 12:01:02 -0800

    minor changes

---
+++ make.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+import os
+
+s = os.system
+s('egrep "<h1|<h2" writing-games.html > /tmp/table.html')
+s('''sed -i 's/name=./href="writing-games.html#/g' /tmp/table.html''')
+
+fp = file('table.html', 'w')
+fp.write('''\
+<html>
+<head>
+<style>
+h1 {
+	font-size: small;
+}
+h2 {
+	font-size: smaller;
+	text-indent: 4em;
+}
+h3 {
+	font-size: normal;
+	text-indent: 8em;
+}
+</style>
+</head>
+<body>
+''')
+fp.close()
+s('cat /tmp/table.html >> ./table.html')
+
+s('cd examples; tar -cv --exclude-vcs example2 > example2.tar')
+s('cd examples; gzip example2.tar')
+s('cd examples; tar -cv --exclude-vcs example3 > example3.tar')
+s('cd examples; gzip example3.tar')
+s('cd examples; tar -cv --exclude-vcs example4 > example4.tar')
+s('cd examples; gzip example4.tar')
+
+
+# always append a '/' on the src directory when rsyncing
+s('rsync -r ./ sjbrowngeeky@ezide.com:/home/sjbrowngeeky/ezide.com/games')
