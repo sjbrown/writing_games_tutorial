aa8ba4d9f5f5c767f950131580397a597ae55027
Shandy Brown <github@ezide.com>
2011-07-09 10:53:50 -0700

    organized make.py

--- make.py
+++ make.py
@@ -1,43 +1,49 @@
 #! /usr/bin/env python
 import os
-
 s = os.system
-s('egrep "<h1|<h2" writing-games.html > /tmp/table.html')
-s('''sed -i 's/name=./href="writing-games.html#/g' /tmp/table.html''')
 
-fp = file('table.html', 'w')
-fp.write('''\
-<html>
-<head>
-<style>
-h1 {
-	font-size: small;
-}
-h2 {
-	font-size: smaller;
-	text-indent: 4em;
-}
-h3 {
-	font-size: normal;
-	text-indent: 8em;
-}
-</style>
-</head>
-<body>
-''')
-fp.close()
-s('cat /tmp/table.html >> ./table.html')
+def create_table_of_contents():
+    s('egrep "<h1|<h2" writing-games.html > /tmp/table.html')
+    s('''sed -i 's/name=./href="writing-games.html#/g' /tmp/table.html''')
 
-targen = ('git checkout %(name)s; '
-          'cp -a code_examples %(name)s; '
-          'tar -cv --exclude-vcs %(name)s > %(name)s.tar; '
-          'rm -rf /tmp/%(name)s; '
-          'mv %(name)s /tmp/%(name)s; '
-          'gzip %(name)s.tar; '
-         )
-s(targen % {'name': 'example2'})
-s(targen % {'name': 'example3'})
-s(targen % {'name': 'example4'})
+    fp = file('table.html', 'w')
+    fp.write('''\
+    <html>
+    <head>
+    <style>
+    h1 {
+            font-size: small;
+    }
+    h2 {
+            font-size: smaller;
+            text-indent: 4em;
+    }
+    h3 {
+            font-size: normal;
+            text-indent: 8em;
+    }
+    </style>
+    </head>
+    <body>
+    ''')
+    fp.close()
+    s('cat /tmp/table.html >> ./table.html')
+
+
+def create_targz_from_example(name):
+    targen = ('git checkout %(name)s; '
+              'cp -a code_examples %(name)s; '
+              'tar -cv --exclude-vcs %(name)s > %(name)s.tar; '
+              'rm -rf /tmp/%(name)s; '
+              'mv %(name)s /tmp/%(name)s; '
+              'gzip %(name)s.tar; '
+             )
+    s(targen % {'name': name})
+
+create_table_of_contents()
+create_targz_from_example('example2')
+create_targz_from_example('example3')
+create_targz_from_example('example4')
 
 print 'Setting git branch to *master*'
 s('git checkout master')
